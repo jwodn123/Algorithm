09f90066acdb29aa09c9bf9a64b27a8c9576233b
JEON <jwodn123@naver.com>
2023-04-05 16:58:59 +0900

    3가지의 수를 오름차순으로 정렬하는 간단한 알고리즘, 이러한 로직 말고 Arrays.sort(); 메서드를 활용하면 더 간단히 정렬시킬 수 있다.

---
+++ src/com/company/Sort_Application/P1.java
@@ -0,0 +1,44 @@
+package com.company.Sort_Application;
+
+/*
+문제
+동규는 세수를 하다가 정렬이 하고싶어졌다.
+숫자 세 개를 생각한 뒤에, 이를 오름차순으로 정렬하고 싶어 졌다.
+숫자 세 개가 주어졌을 때, 가장 작은 수, 그 다음 수, 가장 큰 수를 출력하는 프로그램을 작성하시오.
+
+입력
+숫자 세 개가 주어진다. 이 숫자는 1보다 크거나 같고, 1,000,000보다 작거나 같다. 이 숫자는 모두 다르다.
+
+출력
+제일 작은 수, 그 다음 수, 제일 큰 수를 차례대로 출력한다.
+* */
+
+import java.util.Scanner;
+
+public class P1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        int arr[] = {n1, n2, n3};
+
+        // 방법 1. 3가지 수만 정렬하면 되므로 비교해서 작은값부터 정렬하는 기법 사용.
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[i+1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
