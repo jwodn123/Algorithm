0b879898fb9f9f6b2cf487f4a555fa7e161bab14
JEON <jwodn123@naver.com>
2023-04-06 16:24:10 +0900

    보물찾기 알고리즘 문제, 답을 얻기 위해서는 간단하게 A는 오름차순, B는 내림차순으로 정렬하여 계산하면 답은 간단히 구할 수 있습니다, 하지만 문제에서 B는 재배열이 안된다 하여 재배열을 하지않고 풀 수 있는 로직을 생각중입니다.

---
+++ src/com/company/Sort_Application/P6.java
@@ -0,0 +1,60 @@
+package com.company.Sort_Application;
+
+/*
+문제
+옛날 옛적에 수학이 항상 큰 골칫거리였던 나라가 있었다. 이 나라의 국왕 김지민은 다음과 같은 문제를 내고 큰 상금을 걸었다.
+길이가 N인 정수 배열 A와 B가 있다. 다음과 같이 함수 S를 정의하자.
+S = A[0] × B[0] + ... + A[N-1] × B[N-1]
+S의 값을 가장 작게 만들기 위해 A의 수를 재배열하자. 단, B에 있는 수는 재배열하면 안 된다.
+S의 최솟값을 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 N이 주어진다. 둘째 줄에는 A에 있는 N개의 수가 순서대로 주어지고,
+셋째 줄에는 B에 있는 수가 순서대로 주어진다. N은 50보다 작거나 같은 자연수이고,
+A와 B의 각 원소는 100보다 작거나 같은 음이 아닌 정수이다.
+
+출력
+첫째 줄에 S의 최솟값을 출력한다.
+*/
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class P6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Integer[] a = new Integer[N];
+        Integer[] b = new Integer[N];
+
+        for(int i = 0; i < N; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < N; i++) {
+            b[i] = sc.nextInt();
+        }
+
+        Arrays.sort(a);
+        //b를 내림차순으로 정렬
+        Arrays.sort(b, Collections.reverseOrder());
+
+        int sum = 0;
+
+        for(int i = 0; i < N; i++) {
+            int mul = a[i] * b[i];
+            sum += mul;
+        }
+
+        System.out.println(sum);
+
+
+
+
+
+
+    }
+}
