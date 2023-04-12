4f778af5860734e89051b822898e1b6205c88e4a
JEON <jwodn123@naver.com>
2023-04-12 21:05:43 +0900

    백준 1316번 문제, 기본 문제를 통한 자바 문법 익히기 사용된 문자를 체크하는 부분에서 시간 소요, boolean 타입 변수 활용과 문자가 나왔을 때 아스키 코드를 활용한 방법을 배울 수 있는 문제였다.

---
+++ src/com/company/basic/P6.java
@@ -0,0 +1,39 @@
+//그룹 단어 체
+
+package com.company.basic;
+
+import java.util.Scanner;
+
+public class P6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int count = 0; //그룹 단어 수
+        for(int i = 0; i < N; i++) {
+            String str = sc.next();
+            boolean[] alphabet = new boolean[26]; //사용된 문자 확인
+            boolean tf = true; //그룹 단어 체크
+
+            for(int j = 0; j < str.length(); j++) {
+                int idx = str.charAt(j) - 97; //97->a
+                System.out.println(idx);
+                if(alphabet[idx]) { //이미 사용된 문자라
+                    if(str.charAt(j) != str.charAt(j-1)) { //연속된 문자가 아니라면
+                        tf = false;
+                        break;
+                    }
+                } else { //이전에 사용되지 않았다면
+                    alphabet[idx] = true;
+                }
+            }
+            if(tf) {
+                count++;
+            }
+
+
+        }
+
+        System.out.println(count);
+    }
+}
